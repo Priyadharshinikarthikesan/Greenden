3b818c399480d2fa80a112bea47c00a6a03d22ed
Priyadharshinikarthikesan <priyadharshinipriya0506@gmail.com>
2025-05-18 19:51:08 +0530

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,11 @@
+var sidenav = document.getElementById("sidenav")
+var menuicon = document.getElementById("menuicon")
+var closeicon = document.getElementById("closeicon")
+
+menuicon.addEventListener("click", function(){
+    sidenav.style.right=0
+})
+
+closeicon.addEventListener("click",function(){
+    sidenav.style.right="-50%"
+})
